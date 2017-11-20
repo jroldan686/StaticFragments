492e13f239cd91b6c1783f31cb01a7d740aec40f
jroldan686 <jroldan686@gmail.com>
2017-11-20 13:00:00 +0100

    StaticFragmentsJRL171120

--- app/src/main/java/com/mycompany/staticfragments/FragmentC.java
+++ app/src/main/java/com/mycompany/staticfragments/FragmentC.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * Created by usuario on 16/11/17.
@@ -13,10 +14,16 @@ import android.view.ViewGroup;
 
 public class FragmentC extends Fragment {
 
+    WebView wbvAbout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_c, container, false);
+        if(view != null) {
+            wbvAbout = (WebView)view.findViewById(R.id.wbvAbout);
+            wbvAbout.loadData(getResources().getString(R.string.about), "text/html", "utf-8");
+        }
         return view;
     }
 }
